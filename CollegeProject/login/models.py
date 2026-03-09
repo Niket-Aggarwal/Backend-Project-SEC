d60ecf5d8f25f6af29cb4e53d8ee6d9edd9ddbfa
Niket-Aggarwal <21.xiia.niketaggarwal@gmail.com>
2026-03-09 18:37:10 +0530

    Restraunt Database created

--- CollegeProject/login/models.py
+++ CollegeProject/login/models.py
@@ -30,3 +30,43 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
+
+
+class Restaurant(models.Model):
+    restaurant_name = models.CharField(max_length=150,primary_key=True)
+    password = models.CharField(
+                max_length=20,unique=True,
+                validators=[MinLengthValidator(6),
+                            RegexValidator(
+                                regex=r'^[A-Za-z0-9@#$%^&+=!]*$',
+                                message="Password can contain letters, numbers and special characters"
+                )],null=False,blank=False)
+    phone_number = models.CharField(
+                max_length=10,unique=True,
+                validators=[RegexValidator(
+                                regex=r'^[0-9]{10}$',
+                                message="Phone number must be exactly 10 digits"
+                )])
+    address = models.TextField()
+    pincode = models.CharField(default="000000",
+                max_length=6,
+                validators=[
+                            RegexValidator(
+                                regex=r'^[0-9]{6}$',
+                                message="Pincode must be exactly 6 digits"
+                )])
+    total_gathering = models.PositiveIntegerField()
+    specification = models.CharField(max_length=100)
+    description = models.TextField()
+    photo1 = models.ImageField(upload_to='restaurant_photos/')
+    photo2 = models.ImageField(upload_to='restaurant_photos/', blank=True, null=True)
+    photo3 = models.ImageField(upload_to='restaurant_photos/', blank=True, null=True)
+    photo4 = models.ImageField(upload_to='restaurant_photos/', blank=True, null=True)
+    photo5 = models.ImageField(upload_to='restaurant_photos/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.restaurant_name
