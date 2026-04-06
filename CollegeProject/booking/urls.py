1ec83e3830a6a8e7d3a7b2ae3d52d195ec545081
Anuranjan Toppo <anuranjant882@gmail.com>
2026-04-06 20:51:10 +0530

    booking page banadiya hai ,logout ,delete sab restaurant ke respective banadiya hai

--- CollegeProject/booking/urls.py
+++ CollegeProject/booking/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.booking_page, name='booking'),
+    path('logout/',views.restaurant_logout,name="restaurant_logout"),
+    path('delete/',views.delete_restaurant,name="delete_restaurant"),
 ]
--- CollegeProject/booking/views.py
+++ CollegeProject/booking/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from login .models import Restaurant
 
 def booking_page(request):
-    return render(request, "booking/booking_page.html")
+    restaurant_name=request.session.get("restaurant_name")
+    if not restaurant_name:
+        return redirect('restaurantauth')
+    restaurant=Restaurant.objects.get(restaurant_name=restaurant_name)
+    context={
+        'restaurant':restaurant
+    }
+    return render(request, "booking/booking_page.html",context)
+
+def restaurant_logout(request):
+    request.session.flush()
+    return redirect('restaurantauth')
+
+def delete_restaurant(request):
+    restaurant_name=request.session.get("restaurant_name")
+
+    if restaurant_name:
+        restaurant=Restaurant.objects.get(restaurant_name=restaurant_name)
+        restaurant.delete()
+
+    request.session.flush()
+    return redirect("restaurantauth")
